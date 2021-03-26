11ddfda7421ed6bacea86b85d52dc2d03ff63763
luc <luc.grandin@edu.devinci.fr>
2021-03-26 21:00:08 +0100

    Deploy tentative 1

--- src/scripts/blockchainData.js
+++ src/scripts/blockchainData.js
@@ -1,5 +1,3 @@
-const Web3 = require('web3');
-const web3 = new Web3(Web3.givenProvider || 'ws://some.local-or-remote.node:8546');
 const ethereum = window.ethereum
 
 export const chainId = ethereum.chainId
