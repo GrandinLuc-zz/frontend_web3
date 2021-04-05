e7ee73c4cd2274d367a53a4334d82c5307e1c70e
luc <luc.grandin@edu.devinci.fr>
2021-04-05 14:37:46 +0200

    last question not working yet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link,
+  useRouteMatch,
+  useParams
 } from "react-router-dom";
 import Button from '@material-ui/core/Button';
 import HomePage from './pages/Home';
@@ -13,6 +15,8 @@ import DoucementPage from "./pages/doucement";
 import TransferPage from "./pages/transfer";
 import HoldersPage from "./pages/holders";
 import './App.css'
+import contract from './scripts/toutDoucement'
+import Web3 from 'web3'
 
 export default function App() {
   return (
@@ -25,6 +29,9 @@ export default function App() {
             <Button id="buttonBig" variant="contained" color="primary"><Link to="/doucement" style={{textDecoration: "none"}}>Tout Doucement</Link></Button>
             <Button id="buttonBig" variant="contained" color="primary"><Link to="/transfer" style={{textDecoration: "none"}}>Transfer Tokens</Link></Button>
             <Button id="buttonBig" variant="contained" color="primary"><Link to="/holders" style={{textDecoration: "none"}}>Token Holders</Link></Button>
+            {/*
+            <Button id="buttonBig" variant="contained" color="primary"><Link to="/addressholders" style={{textDecoration: "none"}}>Token Holders</Link></Button>
+            */}
             <Button id="buttonBig" variant="contained" color="primary" onClick={() => loadWeb3()}>Login with metamask</Button>
         </nav>
 
@@ -42,6 +49,9 @@ export default function App() {
           </Route>
           <Route path="/doucement">
             <Doucement />
+          </Route>
+          <Route path="/addressholders">
+            <AddressHolders/>
           </Route>
           <Route path="/">
             <Home />
@@ -70,4 +80,64 @@ function Transfer() {
 
 function Holders() {
   return <HoldersPage/>;
+}
+
+function AddressHolders() {
+  let match = useRouteMatch();
+
+  return (
+    <div>
+      <Switch>
+        <Route path={`${match.path}/:address`}>
+          <h2>Here</h2>
+        </Route>
+        <Route path={match.path}>
+          <h3 class="dataBox">Please enter an address</h3>
+          <input type="text" id="address"></input>
+          {/* 
+          <Button id="buttonBig" variant="contained" color="primary"><Link to={document.getElementById('address').value} style={{textDecoration: "none"}}>Go</Link></Button>
+          */}
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+function AddressHolder() {
+  let { address } = useParams();
+  const web3 = new Web3(Web3.givenProvider || 'ws://some.local-or-remote.node:8546');
+  const eth = web3.eth
+  const [mounted, setMounted] = useState(false)
+  const [stateName, setStateName] = useState(undefined)
+  const [stateCounter, setStateCounter] = useState(0)
+  const account = async () => { return await eth.getAccounts();}
+  const doucement = new eth.Contract(contract.abi, contract.address, {
+    from: account()[0]
+  })
+  
+  useEffect(() =>{
+    setMounted(true)
+  },[])
+
+  const allTokens = async () => {
+    var tokens = document.getElementById('tokens');
+
+    for (var i = 0; i < stateCounter; i++) {
+        var addr = await doucement.methods.ownerOf(i).call();
+        if (addr === address) {
+            tokens.innerHTML += "<div><img src='https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png' height='70' width='70' alt='Corresponds to the token'></img>" +
+            "<br/>Name : " + stateName + "<br/>Token ID : " + i + "</div><br/>"
+        }
+    }
+  }
+  if(!mounted){
+    setStateName(doucement.methods.name().call())
+    setStateCounter(doucement.methods.tokenCounter().call())
+    allTokens();
+  }
+  // Ici il faut utiliser topicId pour aller chercher les contracts qui correspondent
+  return <div>
+      <div class="dataBox">{address}</div>
+      <div class="dataBox" id="tokens"></div>
+    </div>;
 }
